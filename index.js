e658b42dad0cffcdbb1ab98be81c7e8f0c25b64b
megan cho <meganlisacho@gmail.com>
2021-03-18 22:56:27 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+ cats.push("Ralph");   
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+function prependCat(name) {
+    const newCatss = ["Arnold", ...cats];
+    return newCatss;
+}
+function removeLastCat() {
+    const newLastCat = cats.slice(0, -1)
+    return newLastCat;
+}
+function removeFirstCat() {
+    const newFirstCat = cats.slice(1)
+    return newFirstCat;
+}
